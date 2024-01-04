3c4bff90a05cd128b143ed3195cba47d6c12f57f
Andrei Fokau <andrei.fokau@neutron.kth.se>
2024-01-04 17:32:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print('Hello')
+
+
+import http.server as s
+
+
+class H(s.BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(self.path)
+
+
+with s.ThreadingHTTPServer(('0.0.0.0', 8080), RequestHandlerClass=H) as h:
+    print(h.socket.getsockname())
+    try:
+        h.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        exit(0)
